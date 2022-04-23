8f2cb215f30d05f4c572416449e35bc85cdb0376
gfulmor <gfulmor@asu.edu>
2022-04-23 16:11:38 -0400

    First design pattern done with a character builder, start of game in main, and new gameplay class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
 package main.java;
 
+import characterbuilder.java.Character;
 
 public class Main {
-    
-    
-    
+
+    static boolean NakedCharacter;
+    static boolean CharacterJedi;
+    static String helmet;
+    static String armor;
+    static String pants;
+    static String boots;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Welcome to STAR WARS");
+        System.out.println("Building your character...");
+
+        Character character = Generation.generateCharacter();
+
+        System.out.println("\n\nNow entering the Imperial Citadel, Floor 1...");
+
     }
 }
