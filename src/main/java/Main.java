e71389ee794b1a0ec1fdf6c35263300fcbecb7d4
gfulmor <gfulmor@asu.edu>
2022-04-24 00:15:28 -0400

    adding explanatory comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,9 +21,9 @@ public class Main {
 
         // character health is set to 5000 just for the assignment run as otherwise it
         // will run over 5 minutes with normal gameplay
-        // as a result the character will win every battle, however if I didn't do this
-        // the character would
-        // get sent to floor 1 under 15% health/die and go to floor 1 to -level up
+        // as a result the character will win every battle, however in normal health run
+        // the character would get sent to floor 1 under 15% health/die and go to floor
+        // 1 to level up
         character.setHP(5000);
 
         // Sometimes the program randomly stops, it doesn't terminate but gets "stuck."
